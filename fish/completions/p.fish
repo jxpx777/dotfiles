51b7d9aa7547d34df287fe0a393433d4fb5c2a45
Jamie Phelps <jamie@jamiephelps.com>
2018-03-23 08:14:26 -0500

    Update p command and completions for fish shell

---
+++ fish/completions/p.fish
@@ -0,0 +1,3 @@
+complete --command p --wraps="cd" --arguments '(__fish_complete_directories ~/Projects/	All projects
+                                                __fish_complete_directories ~/go/src/github.com/agilebits/	AgileBits go packages
+                                               )'
