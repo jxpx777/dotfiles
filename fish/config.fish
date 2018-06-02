674f2095ec99afbbec434a8c3a0ffa82882e83f8
Jamie Phelps <jamie@jamiephelps.com>
2018-06-02 18:18:39 -0500

    Update path for additional go bin location

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 set GOPATH $HOME/go
 set PATH ./bin /usr/local/share/git-core/contrib/diff-highlight /usr/local/bin /usr/local/sbin /usr/sbin /usr/local/mysql/bin /usr/local/share/npm/bin $GOPATH/bin $PATH
+set PATH $PATH /usr/local/opt/go/libexec/bin
 if status --is-interactive
 	source $HOME/.config/base16-shell/profile_helper.fish
 	eval sh $HOME/.config/base16-shell/scripts/base16-tomorrow.sh
