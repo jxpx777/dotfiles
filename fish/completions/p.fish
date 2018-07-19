207d65b2a8eb24ef19d4d545463593eb3d63666f
Jamie Phelps <jamie@jamiephelps.com>
2018-07-19 09:20:13 -0500

    Latest changes

--- fish/completions/p.fish
+++ fish/completions/p.fish
@@ -1,3 +1,4 @@
 complete --command p --wraps="cd" --arguments '(__fish_complete_directories ~/Projects/	All projects
-                                                __fish_complete_directories ~/go/src/github.com/agilebits/	AgileBits go packages
+                                                __fish_complete_directories ~/go/src/github.com/agilebits/	AgileBits go packages on Github
+                                                __fish_complete_directories ~/go/src/gitlab.com/1password/	1Password go packages on Gitlab
                                                )'
